7404903c78038aca9350c5095459c5603945dac3
802998369 <steve.k.atkinson@bt.com>
2017-08-21 11:36:26 +0100

    Simple test for the service

---
+++ java-application/src/test/java/com/bt/atkinssk/devcondockerdemo/web/HelloWorldServiceTest.java
@@ -0,0 +1,26 @@
+package com.bt.atkinssk.devcondockerdemo.web;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by 802998369 on 16/08/2017.
+ */
+public class HelloWorldServiceTest
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldServiceTest.class);
+
+    @Test
+    public void shouldCallHelloWorld() throws Exception
+    {
+        HelloWorld result = new HelloWorldService().helloWorld("steve");
+
+        LOGGER.info("{}", result);
+
+        assertThat(result.getContent(), is("Hello steve"));
+    }
+}
